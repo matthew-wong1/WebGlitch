d5c0a25cae256bcbd69bf7c54702adbca8a39f4e
Matthew Wong <matthew.wong1@outlook.com>
2024-05-18 13:45:09 +0100

    add test dawn API calls to check if dawn built correctly

---
+++ wgpu_playground/hello_triangle.js
@@ -0,0 +1,9 @@
+const { create, globals } = require('../dawn/out/Debug/dawn.node');
+Object.assign(globalThis, globals); // Provides constants like GPUBufferUsage.MAP_READ
+let navigator = { gpu: create([]), };
+
+if (!navigator.gpu) {
+    throw new Error("WebGPU not supported on this browser.");
+}
+
+console.log("Reached here!");

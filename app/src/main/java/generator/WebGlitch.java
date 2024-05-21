a2c1e1601c064c26c9802b40ffc53056abf0a96a
Matthew Wong <matthew.wong1@outlook.com>
2024-05-21 17:52:40 +0100

    add main program as entry point to program generation

--- app/src/main/java/generator/WebGlitch.java
+++ app/src/main/java/generator/WebGlitch.java
@@ -1,3 +1,25 @@
 package generator;
 
-public class WebGlitch {}
+import static java.lang.Integer.parseInt;
+
+public class WebGlitch {
+
+    private static final Generator generator = new Generator();
+
+    public static void main(String[] args) {
+        // Number of files to generate. Max num lines. Allow optional params (default=true)
+
+        if (args.length != 2) {
+            System.out.println("Usage: ./webglitch <no. files> <max API calls>");
+            System.exit(1);
+        }
+
+        int numPrograms = parseInt(args[0]);
+        int maxCalls = parseInt(args[1]);
+
+        for (int i = 0; i < numPrograms; i++) {
+            generator.generateProgram(maxCalls, i);
+        }
+
+    }
+}

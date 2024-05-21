de08b8594502fa6fd13d4e0ba6693549d9762537
Matthew Wong <matthew.wong1@outlook.com>
2024-05-21 17:57:54 +0100

    remove debugging print statements

--- app/src/main/java/ast/Parser.java
+++ app/src/main/java/ast/Parser.java
@@ -60,8 +60,6 @@ public class Parser {
               rootASTNode = newRootNode;
             }
 
-            System.out.println(rootASTNode.toString());
-
             return rootASTNode;
         }
 

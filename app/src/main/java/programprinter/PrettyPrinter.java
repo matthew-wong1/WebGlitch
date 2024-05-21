7542d3f0075d939cb6a8aa009ffb1b48bd7a582b
Matthew Wong <matthew.wong1@outlook.com>
2024-05-21 17:53:07 +0100

    add class to convert AST to JS file

--- app/src/main/java/programprinter/PrettyPrinter.java
+++ app/src/main/java/programprinter/PrettyPrinter.java
@@ -1,3 +1,36 @@
 package programprinter;
 
-public class PrettyPrinter {}
+import ast.ASTNode;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class PrettyPrinter {
+    private final String OUT_DIRECTORY_PATH = "./output/";
+    public void printToFile(ASTNode root, Integer fileNum) {
+
+        File outFile = new File(OUT_DIRECTORY_PATH + fileNum.toString() + ".js");
+
+        try {
+            if (outFile.createNewFile()) {
+                PrintWriter out = new PrintWriter(outFile);
+                out.println(root.toString());
+                out.close();
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Failed to find file " + fileNum.toString() + ": " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("Failed to create file " + fileNum.toString() + ": " + e.getMessage());
+        }
+
+
+        // String program = format(root.toString());
+    }
+
+    private void format(String path) {
+
+    }
+}

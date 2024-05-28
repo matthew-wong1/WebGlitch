c3d1c75e1ec247105cf7257d5e4dbf286623d712
Matthew Wong <matthew.wong1@outlook.com>
2024-05-28 12:30:20 +0100

    add skeleton js file

--- rsrcs/js/node_skeleton.js
+++ rsrcs/js/node_skeleton.js
@@ -1,5 +1,5 @@
 const puppeteer = require('puppeteer');
-const { create, globals } = require('../../dawn/out/Debug/dawn.node');
+const { create, globals } = require('../../../dawn/out/Debug/dawn.node');
 Object.assign(globalThis, globals);
 let navigator = { gpu: create([]), };
 
@@ -10,4 +10,14 @@ if (!navigator.gpu) {
 async function main() {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-}
+
+    await page.goto("../rsrcs/html/index.html");
+
+    const canvas = document.querySelector('canvas');
+    const context = canvas.getContext('webgpu');
+
+    const devicePixelRatio = window.devicePixelRatio;
+    canvas.width = canvas.clientWidth * devicePixelRatio;
+    canvas.height = canvas.clientHeight * devicePixelRatio;
+    const presentationFormat = navigator.gpu.getPreferredCanvasFormat();
+

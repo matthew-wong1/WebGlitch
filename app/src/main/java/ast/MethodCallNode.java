9c727b3f1a4d3118957a05f8d2be765b9039c095
Matthew Wong <matthew.wong1@outlook.com>
2024-05-29 22:13:39 +0100

    update method call node to generate parameters from tis constructor

--- app/src/main/java/ast/MethodCallNode.java
+++ app/src/main/java/ast/MethodCallNode.java
@@ -1,29 +1,51 @@
 package ast;
 
+import com.fasterxml.jackson.databind.JsonNode;
+
 import java.util.stream.Collectors;
 
 public class MethodCallNode extends ASTNode {
 
-  private final boolean jsonParams;
-  private final String callName;
+    private final boolean jsonParams;
+    private final String callName;
 
-  public MethodCallNode(String receiver, String methodName, boolean jsonParams) {
-    this.callName = receiver + "." + methodName;
-    this.jsonParams = jsonParams;
-  }
+    public MethodCallNode(String receiver, String methodName, boolean jsonParams, JsonNode paramsJsonNode) {
+        this.callName = receiver + "." + methodName;
+        this.jsonParams = jsonParams;
 
-  @Override
-  public String toString() {
-    String delimiter = ", ";
-
-    String parameters;
-    if (jsonParams) {
-      parameters =
-          subnodes.stream().map(ASTNode::toString).collect(Collectors.joining(delimiter, "{", "}"));
-    } else {
-      parameters = subnodes.stream().map(ASTNode::toString).collect(Collectors.joining(delimiter));
+        this.generateParams(paramsJsonNode, this.jsonParams);
     }
 
-    return this.callName + "(" + parameters + ");";
-  }
+    @Override
+    public String toString() {
+        String delimiter = ", ";
+
+        String parameters;
+        if (jsonParams) {
+            parameters =
+                    subnodes.stream().map(ASTNode::toString).collect(Collectors.joining(delimiter, "{", "}"));
+        } else {
+            parameters = subnodes.stream().map(ASTNode::toString).collect(Collectors.joining(delimiter));
+        }
+
+        return this.callName + "(" + parameters + ");";
+    }
+
+    private void generateParams(JsonNode paramsJsonNode, boolean jsonParams) {
+        for (JsonNode param : paramsJsonNode) {
+            param
+                    .fieldNames()
+                    .forEachRemaining(
+                            fieldName -> {
+                                JsonNode paramDetails = param.get(fieldName);
+
+                                //                    if (paramDetails.has("optional")) {
+                                //
+                                //                    }
+
+                                this.addNode(new ParameterNode(fieldName, paramDetails, jsonParams));
+                            });
+        }
+    }
+
 }

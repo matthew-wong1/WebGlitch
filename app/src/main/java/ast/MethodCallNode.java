740cc4af11d07dd67a2c0a449d364bda152da230
Matthew Wong <matthew.wong1@outlook.com>
2024-06-11 18:50:47 +0100

    move parameter generator to ParameterListNode class

--- app/src/main/java/ast/MethodCallNode.java
+++ app/src/main/java/ast/MethodCallNode.java
@@ -7,48 +7,19 @@ import java.util.stream.Collectors;
 
 public class MethodCallNode extends ASTNode {
 
-    private final boolean jsonParams;
     private final String callName;
-    private final Generator generator;
 
     public MethodCallNode(String receiver, String methodName, boolean jsonParams, JsonNode paramsJsonNode, Generator generator) {
         this.callName = receiver + "." + methodName;
-        this.jsonParams = jsonParams;
-        this.generator = generator;
-
-        this.generateParams(paramsJsonNode, this.jsonParams);
+        ParameterListNode parameterListNode = new ParameterListNode(paramsJsonNode, jsonParams, generator);
+        parameterListNode.generateParams();
+        this.addNode(parameterListNode);
     }
 
     @Override
     public String toString() {
-        String delimiter = ", ";
-
-        String parameters;
-        if (jsonParams) {
-            parameters =
-                    subnodes.stream().map(ASTNode::toString).collect(Collectors.joining(delimiter, "{", "}"));
-        } else {
-            parameters = subnodes.stream().map(ASTNode::toString).collect(Collectors.joining(delimiter));
-        }
-
-        return this.callName + "(" + parameters + ");";
+        return this.callName + "(" + subnodes.getFirst().toString() + ");";
     }
 
-    private void generateParams(JsonNode paramsJsonNode, boolean jsonParams) {
-        for (JsonNode param : paramsJsonNode) {
-            param
-                    .fieldNames()
-                    .forEachRemaining(
-                            fieldName -> {
-                                JsonNode paramDetails = param.get(fieldName);
-
-                                //                    if (paramDetails.has("optional")) {
-                                //
-                                //                    }
-
-                                this.addNode(new ParameterNode(fieldName, paramDetails, jsonParams, generator));
-                            });
-        }
-    }
 
 }
---
+++ app/src/main/java/ast/ParameterListNode.java
@@ -0,0 +1,52 @@
+package ast;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import generator.Generator;
+
+import java.util.stream.Collectors;
+
+public class ParameterListNode extends ASTNode {
+    private final Generator generator;
+    private final boolean jsonParams;
+    private final JsonNode paramsJsonNode;
+
+    public ParameterListNode(JsonNode paramsJsonNode, boolean jsonParams, Generator generator) {
+        this.generator = generator;
+        this.jsonParams = jsonParams;
+        this.paramsJsonNode = paramsJsonNode;
+    }
+
+    @Override
+    public String toString() {
+        String delimiter = ", ";
+
+        String parameters;
+        if (jsonParams) {
+            parameters =
+                    subnodes.stream().map(ASTNode::toString).collect(Collectors.joining(delimiter, "{", "}"));
+        } else {
+            parameters = subnodes.stream().map(ASTNode::toString).collect(Collectors.joining(delimiter));
+        }
+
+        return parameters;
+    }
+
+    public void generateParams() {
+
+
+        for (JsonNode param : paramsJsonNode) {
+            param
+                    .fieldNames()
+                    .forEachRemaining(
+                            fieldName -> {
+                                JsonNode paramDetails = param.get(fieldName);
+
+                                //                    if (paramDetails.has("optional")) {
+                                //
+                                //                    }
+
+                                this.addNode(new ParameterNode(fieldName, paramDetails, jsonParams, generator));
+                            });
+        }
+    }
+}

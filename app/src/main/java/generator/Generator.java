655beba2013641d9a8d66b53788a3caf9b9c30d8
Matthew Wong <matthew.wong1@outlook.com>
2024-06-12 17:15:17 +0100

    fix issue with json formatting

--- app/src/main/java/generator/Generator.java
+++ app/src/main/java/generator/Generator.java
@@ -42,7 +42,7 @@ public class Generator {
     }
 
     public static void main(String[] args) {
-        Generator generator = new Generator(20, false);
+        Generator generator = new Generator(100, false);
         generator.generateProgram(1);
     }
 

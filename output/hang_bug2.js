81eaa884c3b0c19aee591a6a189ec8589f50b016
Matthew Wong <matthew.wong1@outlook.com>
2024-07-11 21:45:02 +0100

    add interesting files

---
+++ output/hang_bug2.js
@@ -0,0 +1,44 @@
+const {
+    create,
+    globals
+} = require('../../dawn/out/Debug/Debug/dawn.node');
+const fs = require('fs');
+Object.assign(globalThis, globals);
+let navigator = {
+    gpu: create([]),
+};
+
+if (!navigator.gpu) {
+    throw new Error("WebGPU not supported on this browser");
+}
+
+function loadShader(file) {
+    try {
+        const data = fs.readFileSync(file, 'utf8');
+        return data;
+    } catch (err) {
+        console.error('Failed to load shader:', err);
+    }
+}
+const computeShader = loadShader('../WebGlitch/rsrcs/shaders/compute/2.wgsl');
+
+async function main() {
+    const adapter = await navigator.gpu.requestAdapter({
+        powerPreference: "high-performance"
+    });
+    const device = await adapter.requestDevice();
+    const computeShaderModule = device.createShaderModule({
+        label: "compute",
+        code: computeShader
+    });
+    const computePipeline = device.createComputePipeline({
+        compute: {
+            entryPoint: "main",
+            module: computeShaderModule
+        },
+        label: "computePipeline",
+        layout: "auto"
+    });
+
+}
+main().catch(console.error);

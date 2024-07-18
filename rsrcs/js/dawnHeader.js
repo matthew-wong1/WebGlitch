447395286f77b84d8379b036a31569e7279ba3c4
Matthew Wong <matthew.wong1@outlook.com>
2024-07-19 00:23:08 +0100

    add running the file to generate_and_run and change it into an infinite loop

--- rsrcs/js/dawnHeader.js
+++ rsrcs/js/dawnHeader.js
@@ -1,4 +1,4 @@
-const { create, globals } = require('../../dawn/out/Debug/Debug/dawn.node');
+const { create, globals } = require('D:/final_proj/dawn/out/Debug/Debug/dawn.node');
 Object.assign(globalThis, globals);
 let navigator = { gpu: create([]), };
 const fs = require('node:fs');

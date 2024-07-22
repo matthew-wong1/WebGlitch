483fb1ee11827e14f23c229c259ab5bad4bca5d0
Matthew Wong <matthew.wong1@outlook.com>
2024-07-22 21:06:09 +0100

    add new object to parse config parameters

--- app/src/main/java/generator/WebGlitch.java
+++ app/src/main/java/generator/WebGlitch.java
@@ -14,21 +14,53 @@ public class WebGlitch {
     private static final String DEFAULT_MAX_CALLS = "500";
 
     public static void main(String[] args) {
+        Options options = setupCLIOptions();
+
+        CommandLine cmd = checkCLIOptions(args, options);
+
+        int maxCallsToGenerate = parseInt(cmd.getOptionValue("l", DEFAULT_MAX_CALLS));
+        boolean allowOptionalParams = cmd.hasOption("a");
+        boolean generateMainFunctionOnly = cmd.hasOption("m");
+        Long specificSeed = null;
+        if (cmd.hasOption("s")) {
+            specificSeed = Long.parseLong(cmd.getOptionValue("s"));
+        }
+        boolean wgpuCompatibilityMode = cmd.hasOption("w");
+
+        String filePathToUse = cmd.getOptionValue("o");
+
+        WebGlitchOptions webGlitchOptions = new WebGlitchOptions();
+
+        Generator generator = new Generator(maxCallsToGenerate, allowOptionalParams, wgpuCompatibilityMode, specificSeed, generateMainFunctionOnly);
+
+        generator.generateProgram(filePathToUse);
+    }
+
+    private static CommandLine checkCLIOptions(String[] args, Options options) {
+        CommandLineParser parser = new DefaultParser();
+        CommandLine cmd = null;
+
+        try {
+            cmd = parser.parse(options, args);
+        } catch (ParseException e) {
+            System.err.println(e.getMessage());
+            HelpFormatter formatter = new HelpFormatter();
+            formatter.printHelp("webglitch", options);
+            System.exit(1);
+        }
+        return cmd;
+    }
+
+    private static Options setupCLIOptions() {
         Options options = new Options();
         Option maxCalls = new Option("l", "max-calls", true, "Maximum number of WebGPU API calls per file");
-        Option allowOptional = new Option("a", "allow-optional", false, "Allow optional parameters");
-        Option swarm = new Option("sw", "swarm", false, "Enable swarm testing");
         Option seed = new Option("s", "seed", true, "Seed for RNG");
         Option filePath = new Option("o", "output", true, "Output path");
         Option mainOnly = new Option("m", "mainOnly", false, "Generate only the main function");
         Option wgpuCompatible = new Option("w", "wgpu", false, "Ensures generated programs are wgpu compatible.");
 
-
         maxCalls.setType(Integer.class);
         maxCalls.setRequired(false);
-
-        allowOptional.setType(Boolean.class);
-        allowOptional.setRequired(false);
 
         seed.setType(Long.class);
         seed.setRequired(false);
@@ -43,38 +75,11 @@ public class WebGlitch {
         wgpuCompatible.setType(Boolean.class);
 
         options.addOption(maxCalls);
-        options.addOption(allowOptional);
         options.addOption(seed);
         options.addOption(filePath);
         options.addOption(mainOnly);
         options.addOption(wgpuCompatible);
-
-        CommandLineParser parser = new DefaultParser();
-        CommandLine cmd = null;
-
-        try {
-            cmd = parser.parse(options, args);
-        } catch (ParseException e) {
-            System.err.println(e.getMessage());
-            HelpFormatter formatter = new HelpFormatter();
-            formatter.printHelp("webglitch", options);
-            System.exit(1);
-        }
-
-        int maxCallsToGenerate = parseInt(cmd.getOptionValue("l", DEFAULT_MAX_CALLS));
-        boolean allowOptionalParams = cmd.hasOption("a");
-        boolean generateMainFunctionOnly = cmd.hasOption("m");
-        Long specificSeed = null;
-        if (cmd.hasOption("s")) {
-            specificSeed = Long.parseLong(cmd.getOptionValue("s"));
-        }
-        boolean wgpuCompatibilityMode = cmd.hasOption("w");
-
-        String filePathToUse = cmd.getOptionValue("o");
-
-        Generator generator = new Generator(maxCallsToGenerate, allowOptionalParams, wgpuCompatibilityMode, specificSeed, generateMainFunctionOnly);
-
-        generator.generateProgram(filePathToUse);
+        return options;
     }
 
     public static String getPath() {
--- app/src/main/java/generator/WebGlitchOptions.java
+++ app/src/main/java/generator/WebGlitchOptions.java
@@ -1,2 +1,57 @@
-package generator;public class WebGlitchOptions {
+package generator;
+
+import ast.Parser;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class WebGlitchOptions {
+    private static final String CONFIG_FILE_NAME = "config.json";
+    private final List<String> disabledCalls = new ArrayList<>();
+    private final double invalidParameterChance;
+    private final double percentOfAvailableCallsToGenerate;
+    private final double generateNewRequiredObjectChance;
+    private final int maxGPUDevices;
+
+    public WebGlitchOptions() {
+        ObjectMapper mapper = new ObjectMapper();
+        String filePath = WebGlitch.getPath() + "/" + CONFIG_FILE_NAME;
+        JsonNode configNode = null;
+        try {
+            configNode = mapper.readTree(new File(filePath));
+        } catch (IOException e) {
+            System.err.println("Unable to open config file: " + e.getMessage());
+        }
+
+        Parser.extractNodeAsList(configNode.get("disabledCalls"), disabledCalls);
+        this.invalidParameterChance = configNode.get("invalidParameterChance").asDouble();
+        this.percentOfAvailableCallsToGenerate = configNode.get("percentOfAvailableCallsToGenerate").asDouble();
+        this.maxGPUDevices = configNode.get("maxGPUDevices").asInt();
+        this.generateNewRequiredObjectChance = configNode.get("generateNewRequiredObject").asDouble();
+
+    }
+
+    public List<String> getDisabledCalls() {
+        return disabledCalls;
+    }
+
+    public double getInvalidParameterChance() {
+        return invalidParameterChance;
+    }
+
+    public double getPercentOfAvailableCallsToGenerate() {
+        return percentOfAvailableCallsToGenerate;
+    }
+
+    public double getGenerateNewRequiredObjectChance() {
+        return generateNewRequiredObjectChance;
+    }
+
+    public int getMaxGPUDevices() {
+        return maxGPUDevices;
+    }
 }

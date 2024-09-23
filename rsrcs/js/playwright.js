7af9ebbc80b1c8c819c7e4c7736ab2ef68d1a37b
Matthew Wong <klw19@imperial.ac.uk>
2024-09-23 22:26:34 +0100

    update to puppeteer

--- rsrcs/js/playwright.js
+++ rsrcs/js/playwright.js
@@ -14,27 +14,39 @@ if (!scriptFilePath) {
 (async () => {
     const scriptContent = fs.readFileSync(scriptFilePath, 'utf8');
 
+	// executablePath: '/vol/bitbucket/klw19/chromium/src/out/Default/chrome',
     const browser = await puppeteer.launch({
+		timeout: 60000,
+		protocolTimeout: 60000,
         headless: true,
-        executablePath: '/Users/matthew/Documents/msc/final_proj/WebGlitch/rsrcs/browsers/chromium-131.0.6734.0-mac-asan/Chromium.app/Contents/MacOS/Chromium',
+		executablePath: '/vol/bitbucket/klw19/WebGlitch/rsrcs/browsers/linux/chrome',
         args: [
             '--enable-unsafe-webgpu',
+			'--enable-features=Vulkan',
             '--unsafely-treat-insecure-origin-as-secure=http://localhost:8080'
         ]
     });
 
-    const context = await browser.newContext({
-        ignoreHTTPSErrors: true
-    });
 
-    const page = await context.newPage();
+    // const context = await browser.newContext({ignoreHTTPSErrors: true});
+    // const page = await context.newPage();
 
-    page.on('console', msg => {
-        const text = msg.text();
-        const type = msg.type();
-        const location = msg.location();
-        console.log(`${text}`);
-    });
+	const page = await browser.newPage();
+	
+    // page.on('console', msg => {
+        // const text = msg.text();
+        // const type = msg.type();
+        // const location = msg.location();
+        // console.log(`${text}`);
+    // });
+  page
+    .on('console', message =>
+      console.log(`${message.type().substr(0, 3).toUpperCase()} ${message.text()}`))
+    .on('pageerror', ({ message }) => console.log(message))
+    .on('response', response =>
+      console.log(`${response.status()} ${response.url()}`))
+    .on('requestfailed', request =>
+      console.log(`${request.failure().errorText} ${request.url()}`))
     page.on('pageerror', error => console.error(`[Page Error] ${error.message}`));
     page.on('requestfailed', request => {
         console.log(`Request failed: ${request.failure().errorText} ${request.url()}`);
@@ -46,7 +58,8 @@ if (!scriptFilePath) {
     await page.addScriptTag({ content: scriptContent });
 
     // Wait for the WebGPU code to execute
-    await page.waitForTimeout(5000);
+    // await page.waitForTimeout(5000);
+	await new Promise(r => setTimeout(r, 10000))
 
     // Close the browser
     await browser.close();
